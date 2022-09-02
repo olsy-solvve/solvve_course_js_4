ee96b04898748f32b9b38c80093ddc0937cab466
Ustimov Dmitriy <hccctt@gmail.com>
2022-09-02 18:24:09 +0300

    commit changes

---
+++ server/backend/server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const port = process.env.PORT || 5000
+const app = express();
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
