5cc1b9d3d774a494baed6055128653814a81ac0f
lianka19 <55257879+lianka19@users.noreply.github.com>
2022-09-05 17:46:36 +0300

    #12 add list of animals (#33)
    * clean page
    
    * add list of amimals
    
    * comment error row 77 in router.js
    
    * Change "List With All Pets" to "Animals" in heaed
    
    * front in folder client
    
    * "import dataview" add in prime.js
    
    * add 17 commits
    
    * front -> client + list of animal

--- client/src/views/ListPage.vue
+++ client/src/views/ListPage.vue
@@ -1,3 +1,177 @@
 <template>
-  <div><h1>List with all pets and pagination</h1></div>
+  <section class="section-list">
+    <div class="page-wrapper">
+      <ListOfAllPets
+        :value="pets"
+        :layout="layout"
+        :paginator="true"
+        :rows="9"
+        :sortOrder="sortOrder"
+        :sortField="sortField"
+      >
+        <template #header>
+          <h1>List Of All Pets</h1>
+          <div class="grid grid-nogutter">
+            <div class="col-6" style="text-align: right">
+              <DataViewLayoutOptions v-model="layout"></DataViewLayoutOptions>
+            </div>
+          </div>
+        </template>
+        <template #list="slotProps">
+          <div class="col-12">
+            <div class="pets-list-item">
+              <div class="img-block">
+                <img :src="slotProps.data.img" :alt="slotProps.data.name" />
+              </div>
+              <div class="pets-list-detail">
+                <div class="pets-name">{{ slotProps.data.name }}</div>
+                <div class="status-info">{{ slotProps.data.status }}</div>
+                <div class="period-info">{{ slotProps.data.periodInfo }}</div>
+              </div>
+              <div class="pets-list-action">
+                <MyButton
+                  label="Reply"
+                  class="p-button-raised p-button-rounded"
+                />
+              </div>
+            </div>
+          </div>
+        </template>
+
+        <template #grid="slotProps">
+          <div class="col-12 md:col-4">
+            <div class="pets-grid-item card">
+              <div class="pets-grid-item-top">
+                <span class="status-info">{{ slotProps.data.status }}</span>
+              </div>
+              <div class="pets-grid-item-content">
+                <div class="img-block">
+                  <img :src="slotProps.data.img" :alt="slotProps.data.name" />
+                </div>
+                <div class="pets-name">{{ slotProps.data.name }}</div>
+                <div class="period-info">{{ slotProps.data.periodInfo }}</div>
+              </div>
+              <div class="pets-grid-item-bottom">
+                <MyButton
+                  label="Reply"
+                  class="p-button-raised p-button-rounded but"
+                />
+              </div>
+            </div>
+          </div>
+        </template>
+      </ListOfAllPets>
+    </div>
+  </section>
 </template>
+
+<script>
+import images from "../assets/images.js";
+
+export default {
+  data() {
+    return {
+      pets: [
+        {
+          id: 1,
+          img: images.car01,
+          name: null,
+          status: "Found Pet",
+          periodInfo: "Found 5 days ago",
+          link: "/found",
+        },
+        {
+          id: 2,
+          img: images.car02,
+          name: "Barsik",
+          status: "Lost pet",
+          periodInfo: "Found 12 hours ago",
+          link: "/found",
+        },
+        {
+          id: 3,
+          img: images.car03,
+          name: null,
+          status: "Found pet",
+          periodInfo: "Found 7 days ago",
+          link: "/lost",
+        },
+        {
+          id: 4,
+          img: images.car04,
+          name: null,
+          status: "Found pet",
+          periodInfo: "Found 3 days ago",
+          link: "/found",
+        },
+        {
+          id: 5,
+          img: images.car05,
+          name: "Tom",
+          status: "Lost pet",
+          periodInfo: "Found 2 days ago",
+          link: "/lost",
+        },
+      ],
+      layout: "list",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.page-wrapper {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+.pets-name {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+::v-deep(.pets-list-item) {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  width: 100%;
+  img {
+    width: 150px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    margin-right: 2rem;
+  }
+  .pets-list-detail {
+    flex: 1 1 0;
+  }
+  .pets-list-action {
+    display: flex;
+    flex-direction: column;
+  }
+  .p-button {
+    margin-bottom: 0.5rem;
+  }
+}
+::v-deep(.pets-grid-item) {
+  margin: 0.5rem;
+  border: 1px solid var(--surface-border);
+  .pets-grid-item-top,
+  .pets-grid-item-bottom {
+    display: flex;
+    align-items: center;
+  }
+  .but {
+    left: 45%;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  img {
+    width: 75%;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    margin: 2rem 0;
+  }
+  .pets-grid-item-content {
+    text-align: center;
+  }
+}
+</style>
