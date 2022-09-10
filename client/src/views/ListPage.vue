dd63493c1a915ea0a9815bc958e279d5d227c743
lianka19 <anil12ukr.net@gmail.com>
2022-09-10 21:08:23 +0300

    add discription

--- client/src/views/ListPage.vue
+++ client/src/views/ListPage.vue
@@ -7,35 +7,26 @@
         :paginator="true"
         :rows="4"
       >
-      <!-- :sortOrder="sortOrder" -->
-        <!-- :sortField="sortField" -->
         <template #header>
           <h1>My Pets</h1>
-          <div class="grid grid-nogutter">
+          <!-- <div class="grid grid-nogutter">
             <div class="col-6" style="text-align: right">
-              <!-- <FormDropdown v-model="layout"></FormDropdown> -->
+               <FormDropdown v-model="layout"></FormDropdown> 
             </div>
-          </div>
+          </div> -->
         </template>
 
         <template #list="slotProps">
           <div class="col-12">
-            <div class="pets-list-item">
+            <div class="pets-list-item" @click="openDescriptWindow(slotProps.data)">
               <div class="img-block">
                 <img :src="slotProps.data.img" :alt="slotProps.data.name" />
               </div>
-              <div class="pets-list-detail" @click="openDescriptWindow">
+              <div class="pets-list-detail">
                 <div class = "type-info">Name : <span class="pets-name">{{ slotProps.data.name }}</span></div>
                 <div class = "type-info">Status : <span class="status">{{ slotProps.data.status }}</span></div>
-                <div class = "type-info">Animal : <span class="animal">{{ slotProps.data.animal }}</span></div>
-                <div class = "type-info">Gender : <span class="gender">{{ slotProps.data.gender }}</span></div>
-                <div class = "type-info">Period : <span class="gender">{{ slotProps.data.periodInfo }}</span></div>
-                <div class = "type-info">ID : <span class="id">{{ slotProps.data.id }}</span></div>
               </div>
-              <div class="pets-list-action">
-                <PrimeButton label="Show" icon="pi pi-external-link" v-on:click="openBasic" />
-                
-                
+              <div class="pets-list-action">              
                 <PrimeButton
                   label="Delete pet"
                   icon="pi pi-trash"                  
@@ -46,93 +37,75 @@
             </div>
           </div>  
         </template>
-              
-        
-        <PrimeDialog v-model:visible="openDescriptWindow" >
-          
-          <div class = "type-info">Name : <span class="pets-name">{{ slotProps.data.name }}</span></div>
-                <div class = "type-info">Status : <span class="status">{{ slotProps.data.status }}</span></div>
-                <div class = "type-info">Animal : <span class="animal">{{ slotProps.data.animal }}</span></div>
-                <div class = "type-info">Gender : <span class="gender">{{ slotProps.data.gender }}</span></div>
-                <div class = "type-info">Period : <span class="gender">{{ slotProps.data.periodInfo }}</span></div>
-              
-
-
-          <template #footer>
-            <PrimeButton label="Chahge information of a pet" icon="pi pi-pencil" @click="closeBasic" class="p-button-text" />
-            <PrimeButton label="Close" icon="pi pi-times" @click="closeBasic" autofocus />
-          </template>
-        </PrimeDialog>
-
-<!-- 
-        <PrimeDialog v-model:visible="displayModuleWindow" header="Header" :breakpoints="{ '960px': '75vw', '640px': '90vw' }" :style="{ width: '50vw' }">
-          
-          <div class="field col-12 md:col-4">
-            <label class="type-info">Pet Status</label>
-            <FormDropdown
-              v-model="selectedStatus"
-              :options="status"
-              optionLabel="status"
-              placeholder="Select a Status"
-            ></FormDropdown>
-          </div>
-
-          <div class="field col-12 md:col-4">
-              <label class="type-info">Animal</label>
-              <FormDropdown
-                v-model="selectedAnimal"
-                :options="animal"
-                optionLabel="type"
-                placeholder="Select a type of Animal"
-              ></FormDropdown>
-          </div>
-
-          <div class="field col-12 md:col-4">
-            <label class="type-info">Pet Gender</label>
-              <FormDropdown
-                v-model="selectedGender"
-                :options="genders"
-                optionLabel="gen"
-                placeholder="Select a Genger"
-              ></FormDropdown>
-          </div>
-
-          <div class="field col-12 md:col-4">
-            <label for="description" class="type-info">Date</label>
-              <span class="p-float-label">
-                <InputText id="date" v-model="value2" type="text" />
-                <label for="date">Description </label>
-              </span>
-          </div>
-
-          <template #footer>
-            <PrimeButton
-              label="Cancel"
-              icon="pi pi-times"
-              @click="closeModuleWindow"
-              class="p-button-text"
-            />
-            <PrimeButton
-              label="Submit"
-                icon="pi pi-check"
-                @click="closeModuleWindow"
-                autofocus
-            />
-          </template>
-        </PrimeDialog>  -->
-
-        <PrimeDialog v-model:visible="displayBasic" >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <template #footer>
-            <PrimeButton label="No" icon="pi pi-times" @click="closeBasic" class="p-button-text" />
-            <PrimeButton label="Yes" icon="pi pi-check" @click="closeBasic" autofocus />
-          </template>
-        </PrimeDialog>
-
       </ListOfAllPets>
+
+      <PrimeDialog v-model:visible="displayDescriptWindow" >
+        <div class = "type-info">Name : <span class="pets-name">{{ currentData.name }}</span></div>
+        <div class = "type-info">Status : <span class="status">{{ currentData.status }}</span></div>
+        <div class = "type-info">Animal : <span class="animal">{{ currentData.animal }}</span></div>
+        <div class = "type-info">Gender : <span class="gender">{{ currentData.gender }}</span></div>
+        <div class = "type-info">Period : <span class="gender">{{ currentData.periodInfo }}</span></div>
+        <div class = "type-info">ID : <span class="id">{{ currentData.id }}</span></div>
+        <template #footer>
+          <PrimeButton label="Change information of an pet" icon="pi pi-pensil" @click="openChangeWindow(currentData)"/>
+        </template>
+      </PrimeDialog>
+
+      <PrimeDialog v-model:visible="displayChangeWindow">
+
+
+        <div class="field col-12 md:col-4">
+          <label class="type-info">Pet Status</label>
+          <FormDropdown
+            v-model="selectedStatus"
+            :options="status"
+            optionLabel="status"
+            placeholder="Select a Status"
+          ></FormDropdown>
+        </div>
+
+        <div class="field col-12 md:col-4">
+          <label class="type-info">Animal</label>
+            <FormDropdown
+              v-model="selectedAnimal"
+              :options="animal"
+              optionLabel="type"
+              placeholder="Select a type of Animal"
+            ></FormDropdown>
+        </div>
+
+        <div class="field col-12 md:col-4">
+          <label class="type-info">Pet Gender</label>
+            <FormDropdown
+              v-model="selectedGender"
+              :options="genders"
+              optionLabel="gen"
+              placeholder="Select a Genger"
+            ></FormDropdown>
+        </div>
+
+        <div class="field col-12 md:col-4">
+          <label for="description" class="type-info">Date</label>
+            <span class="p-float-label">
+              <InputText id="date" v-model="value2" type="text" />
+              <label for="date">Description </label>
+            </span>
+        </div>
+        <template #footer>
+          <PrimeButton
+            label="Cancel"
+            icon="pi pi-times"
+            @click="closeModuleWindow"
+            class="p-button-text"
+          />
+          <PrimeButton
+            label="Submit"
+            icon="pi pi-check"
+            @click="closeModuleWindow"
+            autofocus
+          />
+        </template>
+      </PrimeDialog> 
     </div>
   </section>
 </template>
@@ -198,50 +171,44 @@ export default {
       animal: [{ type: "Cat" }, { type: "Dog" }],
       genders: [{ gen: "Female" }, { gen: "Male" }],
       layout: "list",
-      displayModuleWindow: false,
       displayDescriptWindow: false,
+      displayChangeWindow: false,
       selectedName: null,
       selectedStatus: null,
       selectedAnimal: null,
       selectedGender: null,
       //data od period
       //descript
-      displayBasic: false,
     };
   },
   methods: {
 
-    // openModuleWindow() {
-    //   const $name = '#pets-name'
-    //   console.log(this.displayModuleWindow);      
-    //   this.displayModuleWindow = true;
-    //   console.log(this.displayModuleWindow); 
-    // },
-    // closeModuleWindow() {
-    //   this.displayModuleWindow = false;
-    // },
-// ..........................................................................
-    
-    openDescriptWindow(){
-      this.displayModuleWindow = true;
+    openDescriptWindow(data){
+      this.currentData = data;
+      this.displayDescriptWindow = true;
     },
     closeDescriptWindow() {
-      // this.displayModuleWindow = false;
+      this.displayDescriptWindow = false;
     },
 
-    async deleteAnimal(id) {
-      await this.pets.deleteFromList(id);
-      this.showList();
+    openChangeWindow(data) {  
+      this.currentData = data;  
+      this.displayChangeWindow = true;
     },
+    closeChangeWindow() {
+      this.displayChangeWindow = false;
+    },
+    
+  
+    // async deleteAnimal(id) {
+    //   await this.pets.deleteFromList(id);
+    //   this.showList();
+    // },
 
-    openBasic() {
-      console.log(this.displayBasic);
-      this.displayBasic = true;
-      console.log(this.displayBasic);
-    },
-    closeBasic() {
-      this.displayBasic = false;
-    },
+    // showList() {
+    //   this.pets.getPetsList().then((data) => (this.pets = data));
+    // },
+
   },
 };
 </script>
