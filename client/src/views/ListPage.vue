da28bb2cbe12f3325013742737581dd2f43699d2
lianka19 <anil12ukr.net@gmail.com>
2022-09-10 19:33:53 +0300

    еуые

--- client/src/views/ListPage.vue
+++ client/src/views/ListPage.vue
@@ -5,10 +5,10 @@
         :value="pets"
         :layout="layout"
         :paginator="true"
-        :rows="9"
-        :sortOrder="sortOrder"
-        :sortField="sortField"
+        :rows="4"
       >
+      <!-- :sortOrder="sortOrder" -->
+        <!-- :sortField="sortField" -->
         <template #header>
           <h1>My Pets</h1>
           <div class="grid grid-nogutter">
@@ -24,21 +24,49 @@
               <div class="img-block">
                 <img :src="slotProps.data.img" :alt="slotProps.data.name" />
               </div>
-              <div class="pets-list-detail">
-                <div class="type-info">Name:</div>
-                <div class="pets-name">{{ slotProps.data.name }}</div>
-                <div class="status-info">{{ slotProps.data.status }}</div>
-                <div class="period-info">{{ slotProps.data.periodInfo }}</div>
+              <div class="pets-list-detail" @click="openDescriptWindow">
+                <div class = "type-info">Name : <span class="pets-name">{{ slotProps.data.name }}</span></div>
+                <div class = "type-info">Status : <span class="status">{{ slotProps.data.status }}</span></div>
+                <div class = "type-info">Animal : <span class="animal">{{ slotProps.data.animal }}</span></div>
+                <div class = "type-info">Gender : <span class="gender">{{ slotProps.data.gender }}</span></div>
+                <div class = "type-info">Period : <span class="gender">{{ slotProps.data.periodInfo }}</span></div>
+                <div class = "type-info">ID : <span class="id">{{ slotProps.data.id }}</span></div>
               </div>
               <div class="pets-list-action">
-                <MyButton
-                  label="Reply"
+                <PrimeButton label="Show" icon="pi pi-external-link" v-on:click="openBasic" />
+                
+                
+                <PrimeButton
+                  label="Delete pet"
+                  icon="pi pi-trash"                  
                   class="p-button-raised p-button-rounded"
+                  @click="deleteAnimal(slotProps.data.id)"
                 />
               </div>
             </div>
           </div>  
+        </template>
+              
+        
+        <PrimeDialog v-model:visible="openDescriptWindow" >
+          
+          <div class = "type-info">Name : <span class="pets-name">{{ slotProps.data.name }}</span></div>
+                <div class = "type-info">Status : <span class="status">{{ slotProps.data.status }}</span></div>
+                <div class = "type-info">Animal : <span class="animal">{{ slotProps.data.animal }}</span></div>
+                <div class = "type-info">Gender : <span class="gender">{{ slotProps.data.gender }}</span></div>
+                <div class = "type-info">Period : <span class="gender">{{ slotProps.data.periodInfo }}</span></div>
+              
 
+
+          <template #footer>
+            <PrimeButton label="Chahge information of a pet" icon="pi pi-pencil" @click="closeBasic" class="p-button-text" />
+            <PrimeButton label="Close" icon="pi pi-times" @click="closeBasic" autofocus />
+          </template>
+        </PrimeDialog>
+
+<!-- 
+        <PrimeDialog v-model:visible="displayModuleWindow" header="Header" :breakpoints="{ '960px': '75vw', '640px': '90vw' }" :style="{ width: '50vw' }">
+          
           <div class="field col-12 md:col-4">
             <label class="type-info">Pet Status</label>
             <FormDropdown
@@ -55,12 +83,10 @@
                 v-model="selectedAnimal"
                 :options="animal"
                 optionLabel="type"
-                placeholder="Select a typr of Animal"
+                placeholder="Select a type of Animal"
               ></FormDropdown>
           </div>
-        </template>
-                
-        <PrimeDialog>
+
           <div class="field col-12 md:col-4">
             <label class="type-info">Pet Gender</label>
               <FormDropdown
@@ -72,10 +98,10 @@
           </div>
 
           <div class="field col-12 md:col-4">
-            <label for="date" class="type-info">Date</label>
+            <label for="description" class="type-info">Date</label>
               <span class="p-float-label">
                 <InputText id="date" v-model="value2" type="text" />
-                <label for="date">When was pet losted</label>
+                <label for="date">Description </label>
               </span>
           </div>
 
@@ -93,7 +119,19 @@
                 autofocus
             />
           </template>
+        </PrimeDialog>  -->
+
+        <PrimeDialog v-model:visible="displayBasic" >
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <template #footer>
+            <PrimeButton label="No" icon="pi pi-times" @click="closeBasic" class="p-button-text" />
+            <PrimeButton label="Yes" icon="pi pi-check" @click="closeBasic" autofocus />
+          </template>
         </PrimeDialog>
+
       </ListOfAllPets>
     </div>
   </section>
@@ -161,18 +199,48 @@ export default {
       genders: [{ gen: "Female" }, { gen: "Male" }],
       layout: "list",
       displayModuleWindow: false,
+      displayDescriptWindow: false,
       selectedName: null,
       selectedStatus: null,
       selectedAnimal: null,
       selectedGender: null,
+      //data od period
+      //descript
+      displayBasic: false,
     };
   },
   methods: {
-    openModuleWindow() {
+
+    // openModuleWindow() {
+    //   const $name = '#pets-name'
+    //   console.log(this.displayModuleWindow);      
+    //   this.displayModuleWindow = true;
+    //   console.log(this.displayModuleWindow); 
+    // },
+    // closeModuleWindow() {
+    //   this.displayModuleWindow = false;
+    // },
+// ..........................................................................
+    
+    openDescriptWindow(){
       this.displayModuleWindow = true;
     },
-    closeModuleWindow() {
-      this.displayModuleWindow = false;
+    closeDescriptWindow() {
+      // this.displayModuleWindow = false;
+    },
+
+    async deleteAnimal(id) {
+      await this.pets.deleteFromList(id);
+      this.showList();
+    },
+
+    openBasic() {
+      console.log(this.displayBasic);
+      this.displayBasic = true;
+      console.log(this.displayBasic);
+    },
+    closeBasic() {
+      this.displayBasic = false;
     },
   },
 };
@@ -211,26 +279,4 @@ export default {
     margin-bottom: 0.5rem;
   }
 }
-// ::v-deep(.pets-grid-item) {
-//   margin: 0.5rem;
-//   border: 1px solid var(--surface-border);
-//   .pets-grid-item-top,
-//   .pets-grid-item-bottom {
-//     display: flex;
-//     align-items: center;
-//   }
-//   .but {
-//     left: 45%;
-//     margin-top: 20px;
-//     margin-bottom: 20px;
-//   }
-//   img {
-//     width: 75%;
-//     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-//     margin: 2rem 0;
-//   }
-//   .pets-grid-item-content {
-//     text-align: center;
-//   }
-// }
 </style>
