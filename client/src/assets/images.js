346a26e9ff3a1aaab864b45319c88d989db40ae4
lianka19 <anil12ukr.net@gmail.com>
2022-09-12 17:40:40 +0300

    fixed bug in foto of pet

--- client/src/assets/images.js
+++ client/src/assets/images.js
@@ -3,7 +3,7 @@ import car02 from "@/assets/img/img_car_02.jpg";
 import car03 from "@/assets/img/img_car_03.jpg";
 import car04 from "@/assets/img/img_car_04.jpg";
 import car05 from "@/assets/img/img_car_05.jpg";
-import car06 from "@/assets/img/img_car_06.jpg";
+import car06 from "@/assets/img/img_car_07.jpg";
 import dog01 from "@/assets/img/img_home_01.jpg";
 import pet1 from "@/assets/img/pet1.png";
 import main_logo from "@/assets/img/main_logo.png";
